df07be2aef0e45d5454c7455093cfe0477e35426
cyprideux53000 <cyprien.gaudin@supdevinci.edu.fr>
2025-06-12 12:19:38 +0200

    Refactor home.tsx to consolidate React imports

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,5 @@
 'use client';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { PageTitle } from '@/components/PageTitle';
 import { SearchBar } from '@/components/SearchBar';
 import { SearchResults } from '@/components/SearchResults';
