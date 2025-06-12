f2e6961c08528c9f8807459770e418aa92fc6231
cyprideux53000 <cyprien.gaudin@supdevinci.edu.fr>
2025-06-12 12:21:01 +0200

    Refactor RootLayout component to use Readonly type for props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import './styles/global.css';
 
 export default function RootLayout({
                                        children,
-                                   }: {
+                                   }: Readonly<{
     children: React.ReactNode;
-}) {
+}>) {
     return (
         <html lang="fr">
         <body>{children}</body>
